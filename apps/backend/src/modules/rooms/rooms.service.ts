ff119e2d78bb1fac93300ea99f53e057b70a4f1b
Fabio <fabiooosara@gmail.com>
2025-04-25 17:27:11 +0300

    Initial monorepo commit

---
+++ apps/backend/src/modules/rooms/rooms.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Injectable()
+export class RoomsService {
+  constructor(private prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.room.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prisma.room.findUnique({ where: { id } });
+  }
+}
